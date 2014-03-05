add9fe2f9275397c438281fa89cce018d1be2df7
David Vieser <david@vieser.org>
2014-03-04 21:40:46 -0600

    initial commit

---
+++ search.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+    <script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+<!--[if lt IE 8]>
+  		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+ 	<![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<!--<![endif]-->
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>   
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css'>
+  <![endif]-->
+</head>
+
+<body>
+
+  <!--============================== header =================================-->
+<header>
+        <div class="container">
+    <div class="navbar navbar_">
+            <div class="clearfix">
+        <h1 class="brand"><a href="index.html"><img alt="Tagoo" src="img/logo.png"></a></h1>
+        <div class="nav-collapse nav-collapse_  collapse">
+         <ul class="nav sf-menu">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index-1.html">About us </a></li>
+            <li><a href="index-2.html">Clients</a>
+                <ul>
+                    <li><a href="#">Dolore ipsu</a></li>
+                    <li><a href="#">Consecte</a>
+                        <ul>
+                        <li><a href="#">Dolore ipsu</a></li>
+                        <li><a href="#">Consecte</a></li>
+                        <li><a href="#"> Elit Conseq</a></li>
+                       </ul>
+                   </li>
+                   <li><a href="#">Elit Conseq</a></li>
+              </ul>
+            </li>
+            <li><a href="index-3.html">Services</a></li>
+            <li><a href="index-4.html">Mail Us</a></li>
+         </ul>
+              </div>
+      </div>
+          </div>
+  </div>
+      </header>
+
+<!--============================== content =================================-->
+<section id="content">
+  <div class="container">
+    <div class="row">
+    	<div class="span12">
+            <div class="box-cont">
+             <h3>Search result:</h3>
+            <div id="search-results"></div>
+            </div>
+		</div>  
+    </div>        
+
+</div>  
+</section>
+
+<!--============================== footer =================================-->
+<footer>
+        <div class="container clearfix">
+    <div class="pull-right">
+          <!--  <div class="list-soc clearfix">
+             <a href="#"><img alt="" src="img/soc-icon1.png"></a>
+              <a href="#"><img alt="" src="img/soc-icon2.png"></a>
+               <a href="#"><img alt="" src="img/soc-icon3.png"></a> </div> -->
+          </div>
+    <div class="privacy">PRIMARY SIGNAL, LLC. &copy; 2013 | <a href="privacy.html">Privacy Policy</a>&nbsp; 
+            <!-- {%FOOTER_LINK} --></div>
+  </div>
+      </footer>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
